60e7ea7caa6b4bd37137093b0111330e8bb02a8b
Vijay Parmar <vspxcoder@gmail.com>
2020-09-19 21:42:16 +0530

    Add Timer

--- OTPScreen/OTPScreen/ViewController.swift
+++ OTPScreen/OTPScreen/ViewController.swift
@@ -13,9 +13,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var txtopt: UITextField!
     
+    @IBOutlet weak var lbl_timer: UILabel!
     
     @IBOutlet weak var btnsub: UIButton!
+    
+    @IBOutlet weak var btnresend: UIButton!
+    
   
+    var timer:Timer?
+    var count=6
     override func viewDidLoad() {
         super.viewDidLoad()
         txtopt.layer.borderWidth = 1
@@ -26,7 +32,32 @@ class ViewController: UIViewController {
         btnsub.layer.borderWidth = 1
         btnsub.layer.cornerRadius = 10
         btnsub.layer.borderColor = UIColor.white.cgColor
+        btnresend.isHidden=true
+        btnresend.layer.cornerRadius=10
+        btnresend.layer.borderWidth=1
+        btnresend.layer.borderColor=UIColor.white.cgColor
         
+        timer_init()
+       // btnresend.addTarget(self, action: #selector(resendClick), for: .touchUpInside)
+        
+    }
+    func timer_init(){
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(update) , userInfo: nil , repeats: true)
+    }
+    
+  
+   @objc func update(){
+        if count>0 {
+            count-=1
+            lbl_timer.text = String("\(count)")
+            
+            if count == 0
+            {
+                btnresend.isHidden=false
+                timer?.invalidate()
+                count=7
+            }
+        }
     }
     
     @IBAction func text_change(_ sender: Any){
@@ -41,10 +72,19 @@ class ViewController: UIViewController {
         if txtopt.text == "1604"
         {
             txtopt.textColor=UIColor.green
+            btnresend.isHidden=true
+            timer?.invalidate()
+            lbl_timer.isHidden=true
         }else{
             txtopt.textColor=UIColor.red
         }
     }
+    
+    @IBAction func Resnd_click(_ sender: Any) {
+        btnresend.isHidden=true
+         timer_init()
+    }
+    
     
 }
 
